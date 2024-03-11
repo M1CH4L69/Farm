e69be542c2e2fd5adbcb4a03a97b9f20e7c7a33d
nemec6 <misanem06@gmail.com>
2024-03-11 08:33:19 +0100

    Farm class - constructor, getters and setters, method for adding animal, method for printing animal at farm, toString method Dog class - tooString method

--- src/Dog.java
+++ src/Dog.java
@@ -3,4 +3,14 @@ public class Dog extends Animal{
     public Dog(String type, String name, Size size, String specialCare) {
         super(type, name, size, specialCare);
     }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "type='" + type + '\'' +
+                ", name='" + name + '\'' +
+                ", size=" + size +
+                ", specialCare='" + specialCare + '\'' +
+                '}';
+    }
 }
--- src/Farm.java
+++ src/Farm.java
@@ -1,5 +1,50 @@
+import java.util.ArrayList;
+
 public class Farm {
+    private String name;
+    private ArrayList<Animal> list;
 
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Animal> getList() {
+        return list;
+    }
+
+    public void setList(ArrayList<Animal> list) {
+        this.list = list;
+    }
+
+    public Farm(String name) {
+        this.name = name;
+        this.list = new ArrayList<>();
+    }
+
+    public boolean addAnimal(Animal animal){
+        if (list.add(animal)){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public void printFarm(){
+        for (int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Farm{" +
+                "name='" + name + '\'' +
+                ", list=" + list +
+                '}';
+    }
 }
