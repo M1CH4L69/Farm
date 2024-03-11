b26be8df99420f187c16bf5c3b07d44b4595c583
nemec6 <misanem06@gmail.com>
2024-03-11 09:09:34 +0100

    x

--- src/Dog.java
+++ src/Dog.java
@@ -3,7 +3,6 @@ public class Dog extends Animal{
     public Dog(String type, String name, Size size, String specialCare) {
         super(type, name, size, specialCare);
     }
-
     @Override
     public String toString() {
         return "Dog{" +
