a4746d9989e6ad5e640c1bda47538cda498b7464
nemec6 <misanem06@gmail.com>
2024-03-11 09:08:38 +0100

    example ion Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
+        Dog dog = new Dog("doga", "Arnold", Size.BIG, "fasd");
+        Dog dog2 = new Dog("doggy", "Max", Size.SMALL, "kgniu");
+
+        Rose rose = new Rose("rose", 100, 10, 5.5);
+        Farm farm = new Farm("FF");
+
+        System.out.println(farm.addAnimal(dog));
+        System.out.println(farm.addAnimal(dog2));
+        System.out.println(farm.addFlower(rose));
+
+        farm.printFarm("flower");
+        farm.printFarm("Animal");
     }
 }
